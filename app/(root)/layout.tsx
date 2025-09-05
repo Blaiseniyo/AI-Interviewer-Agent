afb515453fe54175a578cca18fec60fb0cdf1528
Noella Chartine <noellachartine@Noellas-MacBook-Pro.local>
2025-09-05 12:37:20 +0200

    fix: change logo on auth pages

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,15 +3,23 @@ import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
-import { isAuthenticated } from "@/lib/actions/auth.action";
+import { isAuthenticated, signOut } from "@/lib/actions/auth.action";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) redirect("/sign-in");
 
+const logout = async () => {
+  'use server'
+  await signOut();
+  redirect("/sign-in");
+};
+
   return (
     <div className="root-layout">
-      <nav>
+      <nav className="flex justify-between items-center p-4">
         <Link href="/" className="">
           <Image
             src="/Logo-AmaliTech.svg"
@@ -21,6 +29,12 @@ const Layout = async ({ children }: { children: ReactNode }) => {
             className="object-cover"
           />
         </Link>
+        <form action={logout}>
+        <Button type="submit" className="bg-gray-500 text-white hover:bg-primary-200">
+          <LogOut className="mr-2  h-4 w-4" />
+          Logout
+        </Button>
+        </form>
       </nav>
 
       {children}
