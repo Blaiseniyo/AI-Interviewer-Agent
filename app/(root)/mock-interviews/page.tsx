5b0a9636d2e11931809a7e3ae7cec0b38e550dea
Noella Chartine <noellachartine@Noellas-MacBook-Pro.local>
2025-09-17 15:47:52 +0200

    fix: pass userId to mock interviews

--- app/(root)/mock-interviews/page.tsx
+++ app/(root)/mock-interviews/page.tsx
@@ -4,19 +4,27 @@ import { getInterviewsByUserId } from '@/lib/actions/general.action';
 import React from 'react'
 
 const page = async () => {
-    const user = await getCurrentUser()
-    const interviews = await getInterviewsByUserId(user?.id);
+  const user = await getCurrentUser()
+  const interviews = await getInterviewsByUserId(user?.id);
 
 
-    console.log(interviews);
+  console.log(interviews);
   return (
     <div>
-        <h1 className='text-2xl font-bold py-2'>Mock Interviews</h1>
-        <div className='interviews-section'>
+      <h1 className='text-2xl font-bold py-2'>Mock Interviews</h1>
+      <div className='interviews-section'>
         {interviews.map((interview) => (
-                <InterviewCard key={interview.id} interview={interview} role={interview.role} type={interview.type} techstack={interview.techstack} createdAt={interview.createdAt} />
-            ))}
-        </div>
+          <InterviewCard
+            key={interview.id}
+            interviewId={interview.id}
+            userId={interview.userId}
+            role={interview.role}
+            type={interview.type}
+            techstack={interview.techstack}
+            createdAt={interview.createdAt}
+          />
+        ))}
+      </div>
     </div>
   )
 }
